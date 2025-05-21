8597cbcdca348da95bf02e1f40d7219592df9e0e
Alex <fornikaras7@gmail.com>
2025-05-21 17:05:30 +0500

    feat: add layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <header>HEADER</header>
+    <Aside>ASIDE SUQA</Aside>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,31 @@
-import AuthPage from '@/features/auth/AuthPage.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: AuthPage,
+      path: '/login',
+      component: AuthLayout,
+      children: [
+        {
+          path: '',
+          name: 'Login',
+          component: () => import('../features/auth/AuthPage.vue'),
+        },
+      ],
     },
     {
-      path: '/recipes',
-      name: 'recipes',
-      component: () => import('../features/recipes/RecipesPage.vue'),
+      path: '/',
+      component: MainLayout,
+      children: [
+        {
+          path: 'recipes',
+          name: 'Recipes',
+          component: () => import('../features/recipes/RecipesPage.vue'),
+        },
+      ],
     },
   ],
 })
