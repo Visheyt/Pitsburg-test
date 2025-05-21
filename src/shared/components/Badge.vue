bf19d699ebc5e48058d5f9238223ab616e991c29
Alex <fornikaras7@gmail.com>
2025-05-21 17:33:04 +0500

    feat: add badge colors and styles

--- src/shared/components/Badge.vue
+++ src/shared/components/Badge.vue
@@ -23,4 +23,25 @@ defineProps<{
   color: #1570ef;
   padding: 3px 6px;
 }
+.easy {
+  border: 1px solid #90fb90;
+  background:
+    linear-gradient(0deg, #90fb90, #90fb90),
+    linear-gradient(0deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));
+  color: #44d54d;
+}
+.medium {
+  border: 1px solid #f5a6231a;
+  background:
+    linear-gradient(0deg, rgba(245, 166, 35, 0.33), rgba(245, 166, 35, 0.33)),
+    linear-gradient(0deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));
+  color: #ffb700;
+}
+.hard {
+  border: 1px solid #f5a6231a;
+  background:
+    linear-gradient(0deg, rgba(248, 51, 51, 0.33), rgba(248, 51, 51, 0.33)),
+    linear-gradient(0deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));
+  color: #f24848;
+}
 </style>
