0230ee32b6d2bab97fbbf59ac4f9799e71699035
Alex <fornikaras7@gmail.com>
2025-05-21 12:56:01 +0500

    feat: add basic files structure, add basic styles , delete useless code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,18 @@
+import AuthPage from '@/features/auth/AuthPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: AuthPage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/recipes',
+      name: 'recipes',
+      component: () => import('../features/recipes/RecipesPage.vue'),
     },
   ],
 })
